73d6a67729ffedf32ba72c65bbcb6c08523b3c7d
Alanete <Alanete93@gmail.com>
2025-05-04 16:53:43 +0300

    Massage on 2 language

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi World");
-        System.out.println("Привет Мир");
+             var dog = 8.0;
+             var cat  = 3.6;
+             var paper = 763789;
+             dog = dog + 4;
+             cat = cat + 4;
+             paper = paper + 4;
+             dog = dog - 3.5;
+             cat = cat - 1.6;
+             paper = paper - 7639;
+             var friend = 19;
+          System.out.println(friend);
+             friend = friend+2;
+             System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalMass = firstBoxer + secondBoxer;
+        System.out.println ("Общая масса бойцов = "+ totalMass + " кг.");
+        var differenceMass = secondBoxer - firstBoxer;
+        System.out.println ("Разница между массой бойцов = "+ differenceMass + " кг.");
+        var remainderDivision = secondBoxer%firstBoxer;
+        System.out.println ("Остаток от деления между двумя весами = "+ remainderDivision + " кг.");
+        var total = 640;
+        var employee = 8;
+        var totalEmployees =total/employee;
+        System.out.println ("Всего работников в компании — "+ totalEmployees + " человек.");
+        totalEmployees = totalEmployees + 94;
+        var total2 = totalEmployees *8;
+        System.out.println ("Если в компании работает "+ totalEmployees + " человек, то всего " + total2 + " часов работы может быть поделено между сотрудниками».");
+
     }
+
 }
