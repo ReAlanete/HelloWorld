eb6370fae76d5ac8da8621db036331bd92db023c
Alanete <Alanete93@gmail.com>
2025-05-04 12:05:21 +0300

    Massage on 2 language

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Very Hi World");
+        System.out.println("Hi World");
+        System.out.println("Привет Мир");
     }
 }
