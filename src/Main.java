205c0d82b0239828ebdf4dcb933ef17c2fa840ad
Alanete <Alanete93@gmail.com>
2025-05-21 00:15:40 +0300

    hw 9 arr2, it was very interesting

--- src/Main.java
+++ src/Main.java
@@ -1,118 +1,58 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
         // Task 1
         System.out.println("Task 1");
-        int salary = 15_000;
-        int total = 0;
-        int month = 0;
-        while (total < 2_459_000) {
-            month++;
-            total = total + salary;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей.");
+        // решение через for each
+        int[] expenses = {55, 77, 22, 11, 33};
+        int expensesMonth = 0;
+        for (int expense : expenses) {
+            expensesMonth += expense;
         }
+        System.out.println("Сумма трат за месяц составила " + expensesMonth + " рублей.");
         // Task 2
+        // нахождение максимума
         System.out.println("Task 2");
-        int i = 0;
-        while (i < 10) {
-            i++;
-            System.out.print(i + " ");
+        int[] expensesWeek = Arrays.copyOf(expenses, expenses.length);
+        int expensesMax = -1;
+        for (int current : expensesWeek) {
+            if (current > expensesMax) {
+                expensesMax = current;
+            }
         }
-        System.out.println();
-        i = 10;
-        for (; i >= 1; i--) {
-
-            System.out.print(i + " ");
+        // нахождение минимума
+        int expensesMin = expensesWeek[0];
+        for (int current : expensesWeek) {
+            if (current < expensesMin) {
+                expensesMin = current;
+            }
         }
-        // Task 3 Решение через while
-        System.out.println(" ");
+        // решение через Arrays.sort (решение для себя)
+        Arrays.sort(expensesWeek);
+        System.out.println(expensesWeek[0] + " min");
+        System.out.println(expensesWeek[expensesWeek.length - 1] + " max");
+        System.out.println("Минимальная сумма трат за неделю составила " + expensesMin + " рублей.");
+        System.out.println("Максимальная сумма трат за неделю составила " + expensesMax + " рублей.");
+        // Task 3
         System.out.println("Task 3");
-        int populationCountryY = 12_000_000;
-        int fertilityPerThousand = 17;
-        int mortalityPerThousand = 8;
-        int year = 2000;
-        System.out.println("Год " + year + " численность населения составляет " + populationCountryY + ".");
-        while (year < 2010) {
-            int populationIncrease = (fertilityPerThousand - mortalityPerThousand) * populationCountryY / 1000;
-            populationCountryY = populationCountryY + populationIncrease;
-            year++;
-            System.out.println("Год " + year + " численность населения составляет " + populationCountryY + ".");
+        int[] expensesWeek2 = Arrays.copyOf(expenses, expenses.length);
+        int totalExpenses = 0;
+        for (int expense : expensesWeek2) {
+            totalExpenses += expense;
         }
-        // Task 3 Решение через for (решение для себя)
-        int populationCountryY1 = 12_000_000;
-        int fertilityPerThousand1 = 17;
-        int mortalityPerThousand1 = 8;
-        int year1 = 2000;
-        System.out.println("Год " + year1 + " численность населения составляет " + populationCountryY1 + ".");
-        for (int i1 = 0; i1 < 10; i1++) {
-            int populationIncrease1 = (fertilityPerThousand1 - mortalityPerThousand1) * populationCountryY1 / 1000;
-            populationCountryY1 = populationCountryY1 + populationIncrease1;
-            year1++;
-            System.out.println("Год " + year1 + " численность населения составляет " + populationCountryY1 + ".");
-        }
+        float expensesAverage = (float) totalExpenses / expensesWeek2.length;
+        System.out.println("Средняя сумма трат за месяц составила " + expensesAverage + " рублей.");
         // Task 4
         System.out.println("Task 4");
-        int salary5 = 15_000;
-        int total5 = 0;
-        float salary6 = salary5 * 0.07F;
-        int month5 = 1;
-        System.out.println("Месяц " + month5 + ", сумма накоплений равна " + total5 + " рублей.");
-        while (total5 < 12_000_000) {
-            month5++;
-            total5 = (int) (total5 + salary6);
-            total5 = (total5 + salary5);
-            System.out.println("Месяц " + month5 + ", сумма накоплений равна " + total5 + " рублей.");
-        }
-        // Task 5
-        System.out.println("Task 5");
-        int salary1 = 15_000;
-        int total1 = 0;
-        float salary2 = salary1 * 0.07F;
-        int month1 = 1;
-        System.out.println("Месяц " + month1 + ", сумма накоплений равна " + total1 + " рублей.");
-        while (total1 < 12_000_000) {
-            month1++;
-            total1 = total1 + salary1;
-            total1 = (int) (total1 + salary2);
-            if (month1 % 6 == 0) {
-                System.out.println("Месяц " + month1 + ", сумма накоплений равна " + total1 + " рублей.");
-            }
-        }
-
-        // Task 6
-        System.out.println("Task 6");
-        int salary3 = 15_000;
-        int total3 = 0;
-        float salary4 = salary3 * 0.07F;
-        int month3 = 1;
-        System.out.println("Месяц " + month3 + ", сумма накоплений равна " + salary3 + " рублей.");
-
-        while (month3 < 108) {
-            month3++;
-            total3 = total3 + salary3;
-            total3 = (int) (total3 + (salary4));
-            if (month3 % 6 == 0)
-                System.out.println("Месяц " + month3 + ", сумма накоплений равна " + total3 + " рублей.");
-        }
-        // Task 7
-        System.out.println("Task 7");
-        int firstFriday = 2;
-        for (; firstFriday < 31; firstFriday += 7) {
-            System.out.println("Сегодня пятница, " + firstFriday + " число. Необходимо подготовить отчет.");
-        }
-        // Task 8
-        System.out.println("Task 8");
-        int currentYear = 2025;
-        int startYear = currentYear - 200;
-        int endYear = currentYear + 100;
-        int period = 79;
-        for (; startYear <= endYear; startYear++) {
-            if (startYear % period == 0) {
-                System.out.println(startYear);
-            }
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.println(reverseFullName[i]);
         }
     }
 }
+
+
 
 
 
