b1be548033bb321367b12af1ff190e94f7996f99
HW-4 <Alanete93@gmail.com>
2025-05-23 23:50:39 +0500

    hw 10 string

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,23 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         // Task 1
         System.out.println("Task 1");
-        // решение через for each
-        int[] expenses = {55, 77, 22, 11, 33};
-        int expensesMonth = 0;
-        for (int expense : expenses) {
-            expensesMonth += expense;
-        }
-        System.out.println("Сумма трат за месяц составила " + expensesMonth + " рублей.");
+        String firstName = " Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
         // Task 2
-        // нахождение максимума
         System.out.println("Task 2");
-        int[] expensesWeek = Arrays.copyOf(expenses, expenses.length);
-        int expensesMax = -1;
-        for (int current : expensesWeek) {
-            if (current > expensesMax) {
-                expensesMax = current;
-            }
-        }
-        // нахождение минимума
-        int expensesMin = expensesWeek[0];
-        for (int current : expensesWeek) {
-            if (current < expensesMin) {
-                expensesMin = current;
-            }
-        }
-        // решение через Arrays.sort (решение для себя)
-        Arrays.sort(expensesWeek);
-        System.out.println(expensesWeek[0] + " min");
-        System.out.println(expensesWeek[expensesWeek.length - 1] + " max");
-        System.out.println("Минимальная сумма трат за неделю составила " + expensesMin + " рублей.");
-        System.out.println("Максимальная сумма трат за неделю составила " + expensesMax + " рублей.");
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName1);
         // Task 3
         System.out.println("Task 3");
-        int[] expensesWeek2 = Arrays.copyOf(expenses, expenses.length);
-        int totalExpenses = 0;
-        for (int expense : expensesWeek2) {
-            totalExpenses += expense;
-        }
-        float expensesAverage = (float) totalExpenses / expensesWeek2.length;
-        System.out.println("Средняя сумма трат за месяц составила " + expensesAverage + " рублей.");
-        // Task 4
-        System.out.println("Task 4");
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. — " + fullName);
 
-        int left = 0;
-        int right = reverseFullName.length - 1; //12
 
-        while (left < right) {                        //0<12       1<11 10
-            char temp = reverseFullName[left];        // temp = 0   1 2
-            reverseFullName[left] = reverseFullName[right]; // = лефт = 12  11 10
-            reverseFullName[right] = temp;                 // темп равен 12  11 10
-            left++;
-            right--;
-        }
-        System.out.println(Arrays.toString(reverseFullName));
     }
 }
 
